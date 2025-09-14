c8c17309f8c09d42ef3c70596c11764d8b9b697c
ben-h-ai <bhicks89@icloud.com>
2025-09-14 17:48:34 +0100

    Add financials analysis - income statement

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("Stock Analysis :chart_with_upwards_trend:")
 
 # Sidebar
 st.sidebar.header("Filters")
-symbol = st.sidebar.text_input("Symbol", "GAW.L")
+symbol = st.sidebar.text_input("Symbol", "NVDA")
 
 # Filters
 period = st.sidebar.segmented_control(label="Period", options=["1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "max"], default="1y")
@@ -37,19 +37,76 @@ day_percent_change = day_change / previous_close
 market_cap = ticker.fast_info.market_cap
 
 # --- Company Overview Section ---
-st.header("Company Info")
-st.subheader(f"{long_name} ({symbol_display})")
+st.header(f"{long_name} ({symbol_display})")
 
 # Business description
+col1, col2 = st.columns(2)
+col1.metric("Sector", sector)
+col2.metric("Industry", industry)
+
 with st.expander("Business Description:"):
     st.write(long_business_summary)
 
 # Key metrics
-col1, col2, col3 = st.columns(3)
+col3, col4, col5 = st.columns(3)
 
-col1.metric("Last Price", f"{currency} {last_price:,.2f}")
-col2.metric("Percent Change", f"{day_percent_change:.1%}")
-col3.metric("Market Cap", f"{currency} {market_cap / 1e9:,.1f} B")
+col3.metric("Last Price", f"{currency} {last_price:,.2f}")
+col4.metric("Percent Change", f"{day_percent_change:.1%}")
+col5.metric("Market Cap", f"{currency} {market_cap / 1e9:,.1f} B")
+
+# --- Financials ---
+
+st.divider()
+st.header("Financials")
+
+financials = ticker.financials
+
+# Income statement
+
+income_statement = ticker.income_stmt
+
+financial_metrics = ["Total Revenue", "Gross Profit", "Operating Income", "Net Income"]
+financial_metric = st.selectbox(label="Financial Metric", options=financial_metrics, index=0, accept_new_options=False)
+
+# Income statement
+financial_metric_data = income_statement.loc[financial_metric]
+# st.subheader(f"{financial_metric}")
+
+df_financial_metric = pd.DataFrame({
+    "date": financial_metric_data.index,
+    "year": financial_metric_data.index.year,
+    financial_metric: financial_metric_data.values
+}).reset_index(drop=True)
+
+financial_metric_chart = (
+    alt.Chart(df_financial_metric)
+    .mark_bar()
+    .encode(
+        # x=alt.X(field="date", type="temporal", timeUnit="year", title="Year"),
+        x=alt.X(field="year", type="ordinal", title="Year", axis=alt.Axis(labelAngle=0)),
+        y=alt.Y(field=financial_metric, type="quantitative", title=financial_metric),
+        tooltip=[alt.Tooltip(field="year", type="ordinal", title="Year"),
+                 alt.Tooltip(field=financial_metric, format=",.0f", title=financial_metric)]
+    )
+)
+
+# theme_base = st.get_option("theme.base")
+# label_colour = "black" if theme_base == "light" else "white"
+#
+# financial_metric_chart_text = (
+#     alt.Chart(df_financial_metric)
+#     .mark_text(dy=-10, color=label_colour)
+#     .encode(
+#         # x=alt.X(field="date", type="ordinal", timeUnit="year", title="Year"),
+#         x=alt.X(field="year", type="ordinal", title="Year", axis=alt.Axis(labelAngle=0)),
+#         y=alt.Y(field=financial_metric, type="quantitative", title=financial_metric),
+#         text=alt.Text(field=financial_metric, type="quantitative", format=",.0f")
+#     )
+# )
+
+st.altair_chart(financial_metric_chart)
+
+# --- Price Analysis ---
 
 # Base Price History Data Frame
 price_history = ticker.history(period=period, interval=interval).reset_index()
@@ -60,7 +117,7 @@ price_history_filtered = price_history[["Date", "Close"] + additional_price_type
 price_history_filtered_melted = price_history_filtered.melt("Date", var_name="Series", value_name="Price")
 
 # Create a selection that follows the mouse
-hover = alt.selection_single(
+hover = alt.selection_point(
     fields=["Date"],
     nearest=True,
     on="mouseover",
